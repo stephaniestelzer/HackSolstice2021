74278b937aee93804ba9634f6f5c257c1114bd53
LVelez1015 <74500568+LVelez1015@users.noreply.github.com>
2021-01-16 13:17:31 -0500

    Update main.cpp
    a few tweaks

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std:: cout << "Hey Team!" << std:: endl;
+    cout << "Hello, World!" << endl;
+    cout << "Hey Team!" << endl;
+
+    cout << "Testing to see if this actually works!" << endl;
     return 0;
 }
